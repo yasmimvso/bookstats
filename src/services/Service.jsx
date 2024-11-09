89a4d176ea11c4fc17f6ae09f924aef642a6a146
yasmimvso <yvso@cin.ufpe.br>
2024-11-09 19:43:40 -0300

    fix: commit obrigatório, adição de comentários*

--- src/services/Service.jsx
+++ src/services/Service.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react';
 import httpClient from './baseAxio'; 
 
 // Service para a consulta de livros
+
+// fazer aquele processo de usar a string, procurar no geral, depois athor, depois editora.
+// se for 
 function Service() {
     // melhorar de modo que analisamos as regras particulares para cada consulta
     const [dado, setDado] = useState([]);
